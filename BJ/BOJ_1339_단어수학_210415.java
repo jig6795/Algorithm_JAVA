b4b851b7d526ad17996b1727dbbcc5657ac3a125
jig6795 <wjd1rb@gmail.com>
2021-04-16 00:14:40 +0900

    210415_BOJ_1339_단어수학[G4]

---
+++ BJ/BOJ_1339_단어수학_210415.java
@@ -0,0 +1,74 @@
+package day0415;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ_1339_단어수학_210415 {
+	
+	static int N, sum; // 단어의 개수, 정답
+	static ArrayList<String> word = new ArrayList<String>();
+	static long[] wordCount = new long[26]; // 등장하는 수를 자리수로 계산
+	static int[] wordSeq = new int[26]; // 단어의 번호
+	static class wordSet implements Comparable<wordSet>{
+		int seq;
+		long count;
+
+		public wordSet(int seq, long count) {
+			this.seq = seq;
+			this.count = count;
+		}
+
+		@Override
+		public int compareTo(wordSet o) {
+			if(this.count > o.count) return -1;
+			else if (this.count == o.count) return 0;
+			else return 1;
+		}
+	}
+	static ArrayList<wordSet> wordList = new ArrayList<wordSet>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < N; i++) {
+			word.add(sc.nextLine());
+		}
+		sc.close();
+
+		// 단어에 해당하는 수 찾기 시작
+		for (int i = 0; i < N; i++) {
+			String w = word.get(i);
+			
+			// 해당 자리수에 해당하는 값 찾기
+			for (int j = 0; j < w.length(); j++) {
+				wordCount[(int)w.charAt(j) - 65] += ((w.length()-j) * Math.pow(10, (w.length()-j-1)));
+			}
+		}
+		
+		for (int i = 0; i < 26; i++) {
+			if(wordCount[i] > 0) {
+				wordList.add(new wordSet(i, wordCount[i]));
+			}
+		}
+		Collections.sort(wordList);
+		
+		// 순서 입력
+		for (int i = 0; i < wordList.size(); i++) {
+			wordSeq[wordList.get(i).seq] = 9-i;
+		}
+		
+		// 수 계산
+		for (int i = 0; i < N; i++) {
+			String w = word.get(i);
+			
+			for (int j = 0; j < w.length(); j++) {
+				sum += (wordSeq[(int) w.charAt(j) - 65] * Math.pow(10, (w.length()-j-1)));
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
